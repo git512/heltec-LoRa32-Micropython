d931aaa6d7b9f4d57fcd8bdf7fcff51e482109f7
git512 <37312706+git512@users.noreply.github.com>
2023-10-24 14:41:54 -0400

    Update LoRaReceiver.py
    Update to V3 Pinout

--- examples/LoRaReceiver.py
+++ examples/LoRaReceiver.py
@@ -2,9 +2,9 @@ from machine import Pin, SoftI2C
 from ssd1306 import SSD1306_I2C
 from time import sleep
 
-oledSDA = Pin(15, Pin.OUT, Pin.PULL_UP)
-oledSCL = Pin(4, Pin.OUT, Pin.PULL_UP)
-oledRST = Pin(16, Pin.OUT)
+oledSDA = Pin(17, Pin.OUT, Pin.PULL_UP)
+oledSCL = Pin(18, Pin.OUT, Pin.PULL_UP)
+oledRST = Pin(21, Pin.OUT)
 oledRST.value(1)
 i2c = SoftI2C(oledSDA, oledSCL)
 oled = SSD1306_I2C(128, 64, i2c)
